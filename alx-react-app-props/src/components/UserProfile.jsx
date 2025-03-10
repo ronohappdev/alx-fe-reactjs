32a817c556b7b4d9737e95d14c961c96249e9774
Oscar <kipngetichronohoscar@gmail.com>
2025-03-10 21:47:10 +0300

    Update UserProfile.jsx

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,9 +1,8 @@
+import { createContext } from 'react';
 const UserProfile = (props) => {
     return (
         <div>
-            <h2>{props.name}</h2>
-            <p>Age: {props.age}</p>
-            <p>Bio: {props.bio}</p>
+            return <UserInfo />;
         </div>
     );
 };
