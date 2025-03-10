5d39bb7b8dcf6b58aab0284e7876a12627d445a2
Oscar <kipngetichronohoscar@gmail.com>
2025-03-10 21:38:50 +0300

    changes

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,19 @@
+import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
+
+import UserProfile from './components/UserProfile'
+import ProfilePage from './components/ProfilePage';
+import UserContext from './components/UserContext';
+
+function App() {
+    const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+  
+    return (
+      <UserContext.Provider value={userData}>
+        <ProfilePage />
+      </UserContext.Provider>
+    );
+  }
+export default App;
